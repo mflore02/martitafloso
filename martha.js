d70d2ab8fcd78659eb2c3860eadd819b79571e24
“Martha” <martha.dfwrealtor@gmail.com>
2018-10-22 19:48:12 -0500

    Added martha javascript

---
+++ martha.js
@@ -0,0 +1,12 @@
+// var x = prompt('please enter your weight in pounds?')
+// var y = x/2.2
+
+// console.log("your weight in Kilos is " + y)
+
+x = 5
+
+if (x === ) {
+	console.log("pt_cruiser")
+} else {
+	console.log("Nope!")
+}
